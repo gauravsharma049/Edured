f5c25cf3bc4328d4ebd6bc432cf4639363ea045e
Gaurav Sharma <gauravsharma049049@gmail.com>
2022-11-08 23:00:56 +0530

    first commit

---
+++ src/main/java/com/company/edured/EduredApplication.java
@@ -0,0 +1,13 @@
+package com.company.edured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduredApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/company/edured/Home/HomeController.java
@@ -0,0 +1,41 @@
+package com.company.edured.Home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("title", "Welcome");
+        return "index";
+    }
+    
+    @GetMapping("/lessons")
+    public String lessons(Model model){
+        model.addAttribute("title", "Lessons");
+        return "lessons";
+    }
+    @GetMapping("/contact-us")
+    public String contact(Model model){
+        model.addAttribute("title", "Contact us");
+        return "contact";
+    }
+    @GetMapping("/blog")
+    public String blog(Model model){
+        model.addAttribute("title", "edured blog");
+        return "blog";
+    }
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "About us");
+        return "about";
+    }
+    @GetMapping("/not-found")
+    public String notfound(Model model){
+        model.addAttribute("title", "Page not found");
+        return "notfound";
+    }
+}
+
