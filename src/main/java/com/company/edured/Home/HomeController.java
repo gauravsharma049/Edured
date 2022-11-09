768dd5ac4eaa7b27fbae12b479ce508bc8a603c0
uk47kumar <uk47kumar@gmail.com>
2022-11-09 11:35:39 +0530

    login page

--- src/main/java/com/company/edured/Home/HomeController.java
+++ src/main/java/com/company/edured/Home/HomeController.java
@@ -37,5 +37,11 @@ public class HomeController {
         model.addAttribute("title", "Page not found");
         return "notfound";
     }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("title","Login");
+        return "login";
+    }
 }
 
